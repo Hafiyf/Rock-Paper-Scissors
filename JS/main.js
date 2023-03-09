6b6e376c3db36bf332c90f8b4ab0dfb5a85055d9
Jamshid <jamshid.oa4k@gmail.com>
2023-03-09 17:45:13 +0500

    update, responsive not working

--- JS/main.js
+++ JS/main.js
@@ -1,63 +1,81 @@
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scis = document.querySelector(".scissors");
-const rpsItem = document.querySelector('.rps__item')
+const rpsItem = document.querySelector(".rps__item");
 const gameUI = document.querySelector(".game__rps");
-const gameUI1 = document.querySelector('.game');
+const gameUI1 = document.querySelector(".game");
 const compChoice = document.querySelector(".comp__choice");
-const againBtn = document.querySelector('.again')
-const compChoiceImg = document.querySelector(".compChoice");
+const againBtn = document.querySelector(".again");
+const compChoiceImg = document.querySelector(".compChoiceImg");
 let scoreValue = document.querySelector(".score__value");
 const winner = document.querySelector(".winner__title");
 const winner2 = document.querySelector(".winner__title2");
 const againBtn2 = document.querySelector(".again2");
+const uPicked = document.querySelector(".u__picked");
+const compPicked = document.querySelector(".comp__picked");
+const loader = document.querySelector(".loader");
 
 let computerOption = "";
 let playerChoice = "";
 let roundWinner = "";
 let score = "";
 
+function playerOption(option){
+  if(option = "rock"){
+    rockChoice()
+  } else if(option = 'paper'){
+    paperChoice()
+  } else if(option = 'scissors'){
+    scisChoice()
+  }
+}
+
 function rockChoice() {
   paper.classList.add("disable");
   scis.classList.add("disable");
-  gameUI.style.background = "none";
-  gameUI.style.width = '20%';
-  gameUI1.style.justifyContent = "space-around";
-  compChoice.classList.remove("disable");
-  againBtn.classList.remove("disable");
-  winner.classList.remove("disable");
   playerChoice = "ROCK";
-
-  playRound();
+  rmDisable();
 }
 
 function paperChoice() {
   rock.classList.add("disable");
   scis.classList.add("disable");
-  gameUI.style.background = "none";
-  gameUI.style.width = '20%'
-  gameUI1.style.justifyContent = "space-around";
-  compChoice.classList.remove("disable");
-  againBtn.classList.remove("disable");
-  winner.classList.remove("disable");
   playerChoice = "PAPER";
-
-  playRound();
+  rmDisable();
 }
 
 function scisChoice() {
   rock.classList.add("disable");
   paper.classList.add("disable");
+  playerChoice = "SCISSORS";
+  rmDisable();
+}
+
+
+function rmDisable(){
   gameUI.style.background = "none";
-  gameUI.style.width = '20%'
+  gameUI.style.width = "20%";
   gameUI1.style.justifyContent = "space-around";
   compChoice.classList.remove("disable");
   againBtn.classList.remove("disable");
   winner.classList.remove("disable");
-  playerChoice = "SCISSORS";
+  uPicked.classList.remove("disable");
+  compPicked.classList.remove("disable");
+  compChoiceImg.setAttribute("src", "./images/Oval.svg");
+  playRound()
+  // setTimeout("playRound()", 1000);
 
-  playRound();
 }
+
+function winnerValue(){
+  ++score;
+  scoreValue.textContent = score;
+  compChoice.style.boxShadow = "none";
+  document.querySelector(".rps__item").style.boxShadow =
+    "0px 0px 0px 45px #ffffff2d, 0px 0px 0px 90px #ffffff1b";
+}
+
+
 
 const choices = ["ROCK", "PAPER", "SCISSORS"];
 
@@ -87,48 +105,49 @@ function playRound() {
   computerOption = computerChoice();
   if (playerChoice === computerOption) {
     roundWinner = "tie";
-    rpsItem.style.boxShadow = 'none';
-    compChoice.style.boxShadow = 'none'
-  }
-  if (
+    winnerValue()
+  } else if (
     (playerChoice === "ROCK" && computerOption === "SCISSORS") ||
     (playerChoice === "SCISSORS" && computerOption === "PAPER") ||
     (playerChoice === "PAPER" && computerOption === "ROCK")
   ) {
     roundWinner = "you win";
-    ++score
-    scoreValue.textContent = score
-    compChoice.style.boxShadow = 'none'
-    rpsItem.style.boxShadow = '2px 4px 0px 25px #fff7002d';
-  }
-  if (
-    (computerOption === "ROCK" && playerChoice === "SCISSORS") ||
-    (computerOption === "SCISSORS" && playerChoice === "PAPER") ||
-    (computerOption === "PAPER" && playerChoice === "ROCK")
-  ) {
+    winnerValue()
+  } else {
     roundWinner = "computer win";
-    rpsItem.style.boxShadow = 'none';
-    compChoice.style.boxShadow = '2px 4px 0px 25px #fff7002d';
+    rpsItem.style.boxShadow = "none";
+    compChoice.style.boxShadow =
+      "0px 0px 0px 45px #ffffff2d, 0px 0px 0px 90px #ffffff1b";
   }
 
   winner.textContent = roundWinner;
   winner2.textContent = roundWinner;
-
 }
 
 
+
+
+
+
+
 // AGAIN btn
-function again(){
+function again() {
   rock.classList.remove("disable");
   paper.classList.remove("disable");
   scis.classList.remove("disable");
   winner.classList.add("disable");
-  againBtn.classList.add("disable");
-  compChoice.classList.add("disable");
-  gameUI.style.background = '';
-  gameUI.style.width = ''
-  gameUI1.style.justifyContent = "center";
-  rpsItem.style.boxShadow = 'none'
-  compChoice.style.boxShadow = 'none'
+  
+  winner.setAttribute('value', '')
 
+  againBtn.classList.add("disable");
+  uPicked.classList.add("disable");
+  compPicked.classList.add("disable");
+
+  gameUI.style.background = "";
+  gameUI.style.width = "";
+  gameUI1.style.justifyContent = "center";
+  rpsItem.style.boxShadow = "none";
+  compChoice.classList.add("disable");
+  compChoiceImg.setAttribute('src', '')
+  compChoice.style.boxShadow = "none";
 }
